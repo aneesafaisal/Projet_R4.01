552d59a1a3dd1a921a8b4cf829581ad640ad0660
lucaviar <luca.andrew.db@gmail.com>
2026-03-27 11:20:39 +0100

    ajouterJoueur

--- FRONTEND/Vue/joueur/ajouter.php
+++ FRONTEND/Vue/joueur/ajouter.php
@@ -27,12 +27,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'
             $_POST['statut']
         )
     ) {
-        header('Location: /joueur');
+        header('Location: ');
     } else {
         error_log("Erreur lors de la création du joueur");
     }
 } else {
-    $formulaire = new Formulaire("/joueur/ajouter");
+    $formulaire = new Formulaire("ajouter");
     $formulaire->setText("Nom", "nom");
     $formulaire->setText("Prenom", "prenom");
     $formulaire->setText("Numéro de license", "numeroDeLicence", "00042");
