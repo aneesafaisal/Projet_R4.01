4b996f0701947119de354df2730ca5a7501f35ab
aneesafaisal <aneesa.amdfaisal@icloud.com>
2026-03-27 10:21:05 +0100

    up

--- FRONTEND/Vue/login.php
+++ FRONTEND/Vue/login.php
@@ -29,7 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["username"]) && isset($
     }
 }
 ?>
-<!-- keep your HTML form as is -->
+
 <body>
     <div class="CentredContainer">
         <h1>Login</h1>
